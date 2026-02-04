eb20fb4d5490888e8f2df3c4d2a4ecde5c3c58d9
Rafael Aquino <rafael.acerqueirah@gmail.com>
2026-02-04 09:44:22 -0300

    Add PDF to Langchain documents

--- app/main.py
+++ app/main.py
@@ -2,3 +2,12 @@ import os
 from dotenv import load_dotenv, find_dotenv
 
 load_dotenv(find_dotenv(), override=True)
+
+
+def load_document(file):
+    from langchain_community.document_loaders import PyPDFLoader
+    print(f'Loading {file}')
+    loader = PyPDFLoader(file)
+    data = loader.load()
+    return data
+
