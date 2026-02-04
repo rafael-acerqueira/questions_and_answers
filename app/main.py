4b858629eaf9f546e3c0a6134d146b9d04e975be
Rafael Aquino <rafael.acerqueirah@gmail.com>
2026-02-04 10:02:00 -0300

    Load data from docx to langchain documents

--- app/main.py
+++ app/main.py
@@ -1,13 +1,22 @@
-import os
 from dotenv import load_dotenv, find_dotenv
 
 load_dotenv(find_dotenv(), override=True)
 
 
 def load_document(file):
-    from langchain_community.document_loaders import PyPDFLoader
-    print(f'Loading {file}')
-    loader = PyPDFLoader(file)
+    import os
+    name, extension = os.path.splitext(file)
+
+    if extension == '.pdf':
+        from langchain_community.document_loaders import PyPDFLoader
+        print(f'Loading {file}')
+        loader = PyPDFLoader(file)
+    elif extension == '.docx':
+        from langchain_community.document_loaders import Docx2txtLoader
+        print(f'Loading {file}')
+        loader = Docx2txtLoader(file)
+    else:
+        print('Document format is not supported!')
+        return None
     data = loader.load()
     return data
-
